be329ca4b3f72d375eb5a0b3384c315cfbd6e38d
marystirling <marystirlingbrown@gmail.com>
2023-08-13 18:02:01 -0500

    command line arguments for image path and timestamp

--- timestamper.py
+++ timestamper.py
@@ -1,14 +1,22 @@
 import os
+import sys
 
-directory = input("Path to images directory: ")
-ts_path = input("Path for timestamp .txt file: ")
-#directory = "/path/to/images/directory"
-# may need to change extension dependent on dataset
-# excludes extension from image name
-image_list = [file[:-4] for file in os.listdir(directory) if file.endswith(".jpg")]
-image_list.sort()
-ts_full_path = ts_path + "/timestamp.txt"
-with open(ts_full_path, "w") as file:
-    for image_name in image_list:
-        file.write(image_name + "\n")
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python3 timestamper.py <path_to_images_directory> <path_for_timestamp_file>")
+        return
+
+    directory = sys.argv[1]
+    ts_path = sys.argv[2]
+
+    image_list = [file[:-4] for file in os.listdir(directory) if file.endswith(".png")]
+    image_list.sort()
+    ts_full_path = os.path.join(ts_path, "timestamp.txt")
+    
+    with open(ts_full_path, "w") as file:
+        for image_name in image_list:
+            file.write(image_name + "\n")
+
+if __name__ == "__main__":
+    main()
 
